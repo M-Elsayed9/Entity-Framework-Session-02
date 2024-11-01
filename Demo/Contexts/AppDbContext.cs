cd3994535821098e5e0a4b30e3281764e7ac3743
Mohamed Elsayed <78699419+M-Elsayed9@users.noreply.github.com>
2024-11-01 02:45:52 +0200

    1. Revision

---
+++ Demo/Contexts/AppDbContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Contexts
+{
+    internal class AppDbContext : DbContext
+    {
+        public DbSet<Entities.Employee> Employees { get; set; }
+        public DbSet<Entities.Department> Departments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = MOHAMED_PC; Database=DemoDB; Trusted_Connection=True; TrustServerCertificate = True; ");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Entities.Employee>().ToTable("Employees");
+            //modelBuilder.Entity<Entities.Department>().ToTable("Departments");
+
+            //modelBuilder.Entity<Entities.Employee>().HasKey(e => e.Id);
+            //modelBuilder.Entity<Entities.Department>().HasKey(d => d.Id);
+
+            //modelBuilder.Entity<Entities.Employee>().Property(e => e.Name).HasMaxLength(50).IsRequired();
+            //modelBuilder.Entity<Entities.Employee>().Property(e => e.Salary).HasColumnType("decimal(18,2)");
+            //modelBuilder.Entity<Entities.Employee>().Property(e => e.Age).IsRequired();
+            //modelBuilder.Entity<Entities.Employee>().Property(e => e.Address).HasMaxLength(100);
+
+            //modelBuilder.Entity<Entities.Department>().Property(d => d.Name).HasMaxLength(50).IsRequired();
+
+            modelBuilder.ApplyConfiguration(new Configurations.EmployeeConfiguration());
+            modelBuilder.ApplyConfiguration(new Configurations.DepartmentConfiguration());
+
+            //modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace Demo
+﻿using Demo.Contexts;
+
+namespace Demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region Revision
+            // EF Core: ORM (Object Relational Mapping) in .NET Core
+
+            // 1. Mapping:
+            //      Code First
+            //      Database First
+
+            // 2. L2E (LINQ to EntityFramework) C# LINQ -> EF Core --> SQL
+
+            // 3 ways to generate 
+            //  1. TPC (Table Per Class) - 1 table for each class
+            //  2. TPH (Table Per Hierarchy) - 1 table for all classes
+            //  3. TPCC (Table Per Concrete Class) - 1 table for each concerete class
+
+            #endregion
+
         }
     }
 }
